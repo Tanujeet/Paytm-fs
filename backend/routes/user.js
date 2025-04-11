e4964cd0f2e79ea9dae56c468be68ed4ea3ca199
Tanujeet <singhtanujeet@gmail.com>
2025-04-11 09:20:59 +0530

    Initial commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,23 @@
 import express from "express";
+import User from "db.js";
+import zod, { string } from "zod";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {});
-
+const signUpSchema = zod.object({
+  username: zod.string(),
+  password: zod.string(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+});
+router.post("/signup", (req, res) => {
+  const body = req.body();
+  const { success } = signUpSchema.safeParse(req.body);
+  if (!success) {
+    return res.json({
+      messgae: "Email already taken/incorrect inputs",
+    });
+  }
+});
 export default router;

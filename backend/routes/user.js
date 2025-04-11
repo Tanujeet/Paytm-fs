0821201e1efa31c0cb79ae49b10dd23cfee43b98
Tanujeet <singhtanujeet@gmail.com>
2025-04-11 10:13:17 +0530

    Initial commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -63,4 +63,26 @@ router.put("/", authMiddleware, async (res, req) => {
     message: "updated successfully",
   });
 });
+
+// ✅ Bulk Search Users (Optional Filter)
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      { firstName: { $regex: filter, $options: "i" } },
+      { lastName: { $regex: filter, $options: "i" } },
+    ],
+  });
+
+  res.json({
+    users: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
 export default router;

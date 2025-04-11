041d810dc57ff7216c7a4c2a2bbbcf594f12dfd1
Tanujeet <singhtanujeet@gmail.com>
2025-04-11 09:32:20 +0530

    Initial commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,19 @@
-import express from "express";
+import express, { json } from "express";
 import User from "db.js";
-import zod, { string } from "zod";
+import zod from "zod";
+import JWT_SECRET from "../config";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {});
 const signUpSchema = zod.object({
-  username: zod.string(),
+  username: zod.string().email(),
   password: zod.string(),
   firstName: zod.string(),
   lastName: zod.string(),
 });
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   const body = req.body();
   const { success } = signUpSchema.safeParse(req.body);
   if (!success) {
@@ -19,5 +21,25 @@ router.post("/signup", (req, res) => {
       messgae: "Email already taken/incorrect inputs",
     });
   }
+
+  const user = User.findOne({
+    username: body.username,
+  });
+  if (!user._id) {
+    return res.json({
+      messgae: "Email already taken/incorrect inputs",
+    });
+  }
+  const dbUser = await User.create(body);
+  const token = jwt.sign(
+    {
+      userId: dbUser._id,
+    },
+    JWT_SECRET
+  );
+  res.json({
+    message: "user created succesfully",
+    token: token,
+  });
 });
 export default router;

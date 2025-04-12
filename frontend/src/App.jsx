46ded495de821b1442daa509288f9d29b32465c1
Tanujeet <singhtanujeet@gmail.com>
2025-04-12 07:54:51 +0530

    frontend update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
-// import { Signin } from "./pages/Signin";
+import { Signin } from "./pages/Signin";
 // import { Dashboard } from "./pages/Dashboard";
 // import { SendMoney } from "./pages/SendMoney";
 
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Heading from "../Components/Heading";
 import { SubHeading } from "../Components/SubHeading";
 
-export const Signup = () => {
+export const Signin = () => {
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-          <Heading label={"sign up"} />
-          <SubHeading label={"Enter your infromation to create an account"} />
+          <Heading label={Signin} />
+          <SubHeading label={"Enter your credentials to access your account"} />
         </div>
       </div>
     </div>

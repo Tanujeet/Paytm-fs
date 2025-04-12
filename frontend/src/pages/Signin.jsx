cdd80329786c389be856150e87000c0581fd6f7c
Tanujeet <singhtanujeet@gmail.com>
2025-04-12 08:09:34 +0530

    frontend update

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -9,6 +9,16 @@ export const Signin = () => {
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={Signin} />
           <SubHeading label={"Enter your credentials to access your account"} />
+          <InputBox placeholder="singhtanujeet@gmail.com" label={"Email"} />
+          <InputBox placeholder="170604" label={"Password"} />
+          <div className="pt-4">
+            <Button label={"Sign in"} />
+          </div>
+          <BottomWarning
+            label={"Don't have an account?"}
+            buttonText={"Sign up"}
+            to={"/signup"}
+          />
         </div>
       </div>
     </div>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Heading from "../Components/Heading";
 import { SubHeading } from "../Components/SubHeading";
+import { Inputbox } from "../Components/Inputbox";
+import { Button } from "../Components/Button";
+import { BottomWarning } from "../Components/BottomWarning";
 
 export const Signup = () => {
   return (
@@ -9,6 +12,18 @@ export const Signup = () => {
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"sign up"} />
           <SubHeading label={"Enter your infromation to create an account"} />
+          <Inputbox placeholder={"Tanujeet"} label={"First Name"} />
+          <Inputbox placeholder={"Singh"} label={"Last Name"} />
+          <Inputbox placeholder={"singhtanujeet2gmail.com"} label={"Email"} />
+          <Inputbox placeholder={"170604"} label={"password"} />
+          <div className="pt-4">
+            <Button label={"Sign Up"} />
+          </div>
+          <BottomWarning
+            label={"Already have an account?"}
+            buttomText={"Sign In"}
+            to={"/singin"}
+          />
         </div>
       </div>
     </div>

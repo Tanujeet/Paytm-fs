c62b063ac25927d16590f7dea1b8733ae769e262
Tanujeet <singhtanujeet@gmail.com>
2025-04-12 08:20:53 +0530

    frontend update

--- frontend/src/Components/Heading.jsx
+++ frontend/src/Components/Heading.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Heading = ({ label }) => {
+export const Heading = ({ label }) => {
   return <div className=" font-bold text-4xl pt-6">{label}</div>;
 };
-
-export default Heading;
